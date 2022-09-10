545a12f7ceaff12b24d753ecacb735b9c2d7ece4
adebisiolayinkam <39669565+adebisiolayinkam@users.noreply.github.com>
2022-09-10 17:21:17 +0100

    step 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -13,6 +13,7 @@ using API.Dtos;
 using AutoMapper;
 using API.Errors;
 using Microsoft.AspNetCore.Http;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -36,15 +37,23 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts(
+          [FromQuery] ProductSpecParams productParams)
         {
-            var spec =  new ProductsWithTypesAndBrandsSpecification();
+            var spec =  new ProductsWithTypesAndBrandsSpecification(productParams);
+             
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countSpec);
 
             var products = await _productsRepo.ListAsync(spec);
 
-            return Ok(_mapper
+            var data = _mapper
             .Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>
-            (products));
+            (products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex,
+             productParams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
