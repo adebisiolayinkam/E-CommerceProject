19af177d8aa8ab91b696be10a6943396ef762330
adebisiolayinkam <39669565+adebisiolayinkam@users.noreply.github.com>
2022-10-04 22:29:23 +0100

    step 14

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './shared/models/pagination';
-import { IProduct } from './shared/models/product';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +9,16 @@ import { IProduct } from './shared/models/product';
 export class AppComponent implements OnInit {
   title = 'E-COMMERCE';
  
-  // products: IProduct[];
- 
-
-  constructor() { }
+  constructor(private basketService: BasketService) { }
    //  constructor(private http: HttpClient) { }
   ngOnInit(): void {
-
-  //   this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-  //     (response: IPagination) => {
-    
-  // // this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((response: any) => {
-  //    this.products = response.data;
-  //   }, error => {
-  //       console.log(error);
-  //   });
-
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
+    }
   }
 }
